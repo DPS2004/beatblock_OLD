89c21de8fa16e66e5b9ca578f8b6757e27542aee
DPS2004 <dpsophinos@gmail.com>
2020-12-16 16:49:27 -0500

    resync shinamon, add more to the level

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
   release = false
   ismobile = false
   mobileoverride = false
-  gameWidth, gameHeight = 400,240 -- WARNING: SETTING THESE TO ANYTHING OTHER THAN 400,240 MAY CAUSE UNDESIRED EFFECTS!!!
+  gameWidth, gameHeight = 400,240- WARNING: SETTING THESE TO ANYTHING OTHER THAN 400,240 MAY CAUSE UNDESIRED EFFECTS!!!
   windowWidth, windowHeight = 800, 480
   t.window.usedpiscale = false
   if not release then
