292ddd849e65e528343289a340761fb48a4442e3
DPS2004 <dpsophinos@gmail.com>
2020-12-21 15:17:12 -0500

    itch.io butler uploader batch file

---
+++ uploadtoitch.bat
@@ -0,0 +1,5 @@
+butler push release/Beatblock-win32.zip monocrash-studios/beatblock:win-32
+butler push release/Beatblock-win64.zip monocrash-studios/beatblock:win-64
+butler push release/Beatblock-mac.zip monocrash-studios/beatblock:mac
+butler push release/beatblock-aligned-debugSigned.apk monocrash-studios/beatblock:android
+butler push release/Beatblock.love monocrash-studios/beatblock:love2d
